b01323f74892af99cd6bf52c8b22e05aa5eb73bc
Deji Adepetun <23409407+dejstheman@users.noreply.github.com>
2021-03-26 23:13:50 +0000

    check if oh my zsh is already installed, create symlink for git template

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
 # install command-line tools
-sudo xcode-select --install
-
-if ! which -s brew; then
-  # Install Homebrew
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if type xcode-select >&- && xpath=$(xcode-select --print-path) &&
+  test -d "${xpath}" && test -x "${xpath}"; then
+  echo "Command line tools already installed"
+else
+  echo "Installing command line tools"
+  sudo xcode-select --install
 fi
 
-brew install pipenv
+# Install Homebrew
+if ! which -s brew; then
+  echo "installing homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Homebrew is already installed"
+fi
 
-# install the dot files
+echo "Installing pipenv"
+
+brew install pipenv
 pipenv install
 pipenv run dotbot -c ./install.conf.yaml
-
-source ./.zshrc
