b186868dd7ef38d1c973274245158196f84929df
Deji Adepetun <23409407+dejstheman@users.noreply.github.com>
2021-05-29 04:39:43 +0100

    add tmux configuration

--- tmux/tpm.sh
+++ tmux/tpm.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-rm -rf ~/.tmux/plugins/tpm
+if [ ! -d  "$HOME/.tmux/plugins/tpm" ]; then
+  echo "Tmux Package Manager does not exist. Cloning it to ~/.tmux/plugins/tpm"
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+  echo "Tmux Package Manager already exists. No need to clone it."
+fi
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Installing plugins from ~/.tmux.conf"
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
